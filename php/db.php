d588993671ff84a908fca663e36164c329d2b75b
zoyagalstyann <galstyanz039@gmail.com>
2025-11-11 16:26:35 +0400

    changes

--- php/db.php
+++ php/db.php
@@ -1,16 +1,18 @@
 <?php
 // php/db.php
-$host = "127.0.0.1";
-$user = "root";      // XAMPP default
-$pass = "";          // XAMPP default empty
-$db   = "restaurant_db";
+$host = "sql106.infinityfree.com";     // from InfinityFree MySQL Hostname
+$user = "if0_40368731";                // MySQL Username
+$pass = "";          // replace with your real MySQL password
+$db   = "if0_40368731_restaurant_db";  // MySQL Database Name
 $port = 3306; 
 
 $mysqli = new mysqli($host, $user, $pass, $db, $port);
 if ($mysqli->connect_errno) {
   http_response_code(500);
-  echo json_encode(["error" => "DB connection failed"]);
+  echo json_encode(["error" => "DB connection failed: " . $mysqli->connect_error]);
   exit;
 }
+
 $mysqli->set_charset("utf8mb4");
 header("Content-Type: application/json; charset=utf-8");
+?>
